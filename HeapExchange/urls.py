d9c92deec0da24293c76621312dc3c232fbb31e3
Daniel Qiu <qsz1328@gmail.com>
2015-04-10 16:14:08 +0800

    app url

--- HeapExchange/urls.py
+++ HeapExchange/urls.py
@@ -10,6 +10,10 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^account/', include('account.urls')),
     url(r'^post/', include('post.urls')),
-    url(r'^headmin/', include('headmin.urls'))
+    url(r'^heap_admin/', include('heap_admin.urls')),
+    url(r'^announcement/', include('announcement.urls')),
+    url(r'^search/', include('search.urls')),
+    url(r'^heap_coin/', include('coin.urls')),
+
 
 ]
