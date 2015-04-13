7447092d8e90336d10b7de0906a72c246bf57ca2
Daniel Qiu <qsz1328@gmail.com>
2015-04-13 14:52:58 +0800

    coin model

--- coin/models.py
+++ coin/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Balance(models.Model):
+    user = models.OneToOneField(User)
+    amount = models.IntegerField()
+
+
+
+
+
+class Transfer(models.Model):
+    from_user = models.OneToOneField(User, related_name="expense")
+    to_user = models.OneToOneField(User, related_name="income")
+    amount = models.IntegerField()
