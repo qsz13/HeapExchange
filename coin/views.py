6217d733c4ef85a6a176905176b5624d2bfc6e7f
XuJian1252878 <1252878xujian@tongji.edu.cn>
2015-04-26 11:10:34 +0800

    about coin history

--- coin/views.py
+++ coin/views.py
@@ -13,3 +13,39 @@ class CoinView(View):
         transfers = Transfer.objects.filter(Q(from_user=request.user)|Q(to_user=request.user))
         return render(request, self.template_name,
                       {"balance": balance,"transfers":transfers})
+
+
+class TransactionView(View):
+    template_name = 'coin/transaction_history.html'
+
+    def get(self, request):
+        balance = request.user.balance.amount
+        transactions = Transfer.objects.filter(Q(from_user=request.user) | Q(to_user=request.user))
+        transaction_count = transactions.count()
+        return render(request, self.template_name, {"page_title": "transaction history", "balance": balance,
+                                                    "transaction_count": transaction_count,
+                                                    "transactions": transactions})
+
+
+class InputTransactionView(View):
+    template_name = "coin/input_transaction.html"
+
+    def get(self, request):
+        balance = request.user.balance.amount
+        transactions = Transfer.objects.filter(Q(to_user=request.user))
+        transaction_count = transactions.count()
+        return render(request, self.template_name, {"page_title": "input transaction", "balance": balance,
+                                                    "transaction_count": transaction_count,
+                                                    "transactions": transactions})
+
+
+class OutputTransactionView(View):
+    template_name = "coin/output_transaction.html"
+
+    def get(self, request):
+        balance = request.user.balance.amount
+        transactions = Transfer.objects.filter(Q(from_user=request.user))
+        transaction_count = transactions.count()
+        return render(request, self.template_name, {"page_title": "output transaction", "balance": balance,
+                                                    "transaction_count": transaction_count,
+                                                    "transactions": transactions})
