0ab3b8f132810b348807c531b039480489b9b11d
Daniel Qiu <qsz1328@gmail.com>
2015-04-29 16:25:36 +0800

    course date picker

--- post/forms.py
+++ post/forms.py
@@ -1,10 +1,9 @@
 from django.forms import ModelForm, Textarea
 from .models import Course, Activity
-from bootstrap3_datetime.widgets import DateTimePicker
 from django import forms
 from functools import partial
 
-DateInput = partial(forms.DateInput, {'class': 'datepicker'})
+DateInput = partial(forms.DateTimeInput, {'class': 'datepicker','type':'date'})
 
 class CourseForm(ModelForm):
     class Meta:
@@ -12,8 +11,8 @@ class CourseForm(ModelForm):
         fields = ['title', 'location', 'time', 'deadline', 'description', 'requirement', 'limit', 'price']
         widgets = {
         	'title' : forms.TextInput(),
-            'time' : DateInput(),
-            'deadline' : DateInput(),
+            # 'time' : DateInput(),
+            # 'deadline' : DateInput(),
             'limit' : forms.NumberInput(),
             'description' : Textarea(attrs = {'cols' : 80, 'rows' : 20, 'class':'materialize-textarea'}),
             'price' : forms.NumberInput(),
