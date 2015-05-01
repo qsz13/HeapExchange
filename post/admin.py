6e879b43954cdffba0abcc5f0666edf7f1bfa780
Daniel Qiu <qsz1328@gmail.com>
2015-05-01 14:02:24 +0800

    course detail avatar

--- post/admin.py
+++ post/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from post.models import Tag
+from post.models import Tag, Course, Activity
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
     pass
