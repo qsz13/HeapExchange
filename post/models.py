5900e5015085bb6f088ccdb4c770ea9b401f82dd
Daniel Qiu <qsz1328@gmail.com>
2015-05-07 09:06:45 +0800

    schedule model, fix select

--- post/models.py
+++ post/models.py
@@ -1,10 +1,49 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 from django.core.urlresolvers import reverse
 from django.db import models
 
 
+
+class OneTimeSchedule(models.Model):
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+
+class SequenceTimeSchedule(models.Model):
+    start_date = models.DateField()
+    end_date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+
+class WeeklyTimeSchedule(models.Model):
+
+    start_date = models.DateField()
+    end_date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+
 class Post(models.Model):
+
+    ONE_TIME = "ONCE"
+    SEQUENCE_TIME = "SEQU"
+    WEEKLY_TIME = "WEEK"
+    CUSTOMIZE_TIME = "CUST"
+
+    SCHEDULE_TYPE = (
+        (ONE_TIME, u"当天结束"),
+        (SEQUENCE_TIME, u"连续多天"),
+        (WEEKLY_TIME, u"每周举行"),
+        (CUSTOMIZE_TIME, u"自定义"),
+    )
+
+
     title = models.CharField(max_length=100, null=True)
     time = models.DateField(null=True)
     description = models.CharField(max_length=500, null=True)
@@ -13,6 +52,11 @@ class Post(models.Model):
     requirement = models.CharField(max_length=500, null=True)
     initialtime = models.DateTimeField(auto_now=True)
     limit = models.IntegerField(null=True)
+    schedule_type = models.CharField(max_length=4, null=True)
+    one_time_schedule = models.ForeignKey(OneTimeSchedule, null=True)
+    sequence_time_schedule = models.ForeignKey(SequenceTimeSchedule, null=True)
+    weekly_time_schedule = models.ForeignKey(WeeklyTimeSchedule, null=True)
+
 
     class Meta:
         abstract = True
@@ -50,3 +94,5 @@ class Tag(models.Model):
 
     def __unicode__(self):
         return self.name
+
+
