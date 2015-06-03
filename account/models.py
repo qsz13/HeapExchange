b271ab188b359f151f94e0480ef0f3ee9ad0135d
Daniel Qiu <qsz1328@gmail.com>
2015-06-03 14:57:29 +0800

    invite link

--- account/models.py
+++ account/models.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import ImageField
@@ -8,22 +11,21 @@ from coin.models import Balance, Transfer
 
 
 class Profile(models.Model):
-
-
     GENDER_CHOICE = (
         ("M", "Male"),
         ("F", "Female"),
     )
 
     user = models.OneToOneField('auth.User')
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICE, null=True, blank=False, default=None)
-    age = models.IntegerField(null=True, blank=True)
-    email = models.EmailField(null=True, blank=True)
-    phone = models.CharField(max_length=13, null=True, blank=True)
-    school = models.CharField(max_length=15, null=True, blank=True)
-    timetable = models.CharField(max_length=77, default="0"*77)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICE, null=True, blank=False, default=None,
+                              verbose_name=u'性别')
+    age = models.IntegerField(null=True, blank=True, verbose_name=u'年龄')
+    email = models.EmailField(null=True, blank=True, verbose_name=u'邮箱')
+    phone = models.CharField(max_length=13, null=True, blank=True, verbose_name=u'电话')
+    school = models.CharField(max_length=15, null=True, blank=True, verbose_name=u'学院')
+    timetable = models.CharField(max_length=77, default="0" * 77)
     interest_tag = models.ManyToManyField('post.Tag', related_name="interest_profile", blank=True)
-    avatar = AvatarField(upload_to='avatar',default="default.png",width=100, height=100)
+    avatar = AvatarField(upload_to='avatar', default="default.png", width=100, height=100)
     referral = models.ForeignKey('auth.User', null=True, related_name='refered_user')
 
 
@@ -44,9 +46,11 @@ def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
+
 def create_user_balance(sender, instance, created, **kwargs):
     if created:
         Balance.objects.create(user=instance, amount=REGISTER_COIN)
+
 
 post_save.connect(create_user_profile, sender=User)
 post_save.connect(create_user_balance, sender=User)
